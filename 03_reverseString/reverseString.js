50c6887d8f08fae6f195630032cbc100f6918033
ravenvst <142888381+ravenvst@users.noreply.github.com>
2023-10-26 13:53:14 +0200

    add string reversing

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringLength = string.length; 
+    let stringReversed = "";
+    const splitStringArray = string.split("");
 
+    while (stringLength > 0){
+        stringReversed += splitStringArray[stringLength-1];
+        stringLength--;
+    }
+
+    return stringReversed;
 };
 
 // Do not edit below this line
