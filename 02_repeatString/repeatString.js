46a6b1bc767a83806f86ba44049a2d23e50b9e33
ravenvst <142888381+ravenvst@users.noreply.github.com>
2023-10-26 09:05:09 +0200

    feat: add function for repeating strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+function repeatString (string, num){
+    if (num < 0) {
+        return "ERROR";
+    }
 
-};
+    let count = 0;
+    let printout ="";
+
+    while (count < num){
+     
+        printout += string;
+        count++;
+
+    }
+
+
+    return printout;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
