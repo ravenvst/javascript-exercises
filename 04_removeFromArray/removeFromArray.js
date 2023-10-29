2ffed72d0694491761f95ff97ed96c8ed8c15259
ravenvst <142888381+ravenvst@users.noreply.github.com>
2023-10-29 12:15:11 +0100

    pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,35 @@
-
-
-
-const removeFromArray = function(myArray, x) {
+const removeFromArray = function() {
+    const filteredArray = [];
+    const valuesToRemove = Array.from(arguments).slice(1);
+    arguments[0].forEach(removeValues);
     
-    const filteredArray = myArray.filter(checkEntry);
-    return filteredArray;
-
-    function checkEntry(entry){
-        return x!==entry;
+    function removeValues (item){
+        for (let i = 0; i < valuesToRemove.length; i ++){
+                if (!valuesToRemove.includes(item) && !filteredArray.includes(item)) {
+                    filteredArray.push(item);
+                    
+                }
+        }
     }
-};
+
+    /*
+    const filteredArray = arguments[0];
+
+    for (let i = 0; i < valuesToRemove.length; i++){
+
+        filteredArray = filteredArray.filter(selectElement);
+        
+        
+       
+        function selectElement(element){
+            return valuesToRemove[i] !== element;
+        }
+        
+    }
+    */
+    return filteredArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
