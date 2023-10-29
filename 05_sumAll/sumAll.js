e39332c2c30118a343ab28bd636a8e4b190d0cb7
ravenvst <142888381+ravenvst@users.noreply.github.com>
2023-10-29 17:34:57 +0100

    pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(fromInt, toInt) {
+    let sum = 0;
 
+    if (fromInt < 0 || toInt < 0 || typeof fromInt !== "number" || typeof toInt !== "number"){
+        return "ERROR";
+    }
+    else if (fromInt < toInt) {
+        for (let i = fromInt; i <= toInt; i ++){
+            sum += i;
+        } 
+    } else {
+        for (let i = fromInt; i >= toInt; i --){
+            sum += i;
+        }
+    }       
+
+    return sum;
 };
 
 // Do not edit below this line
