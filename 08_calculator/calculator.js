7b84413420362a6a6480db10c8383d410986abd4
ravenvst <142888381+ravenvst@users.noreply.github.com>
2023-12-15 18:43:01 +0100

    pass the rest of the tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -10,22 +10,31 @@ const subtract = function(x, y) {
 
 const sum = function() {
   let result = 0;
-  for (let i = 0; i < arguments.length; i++) {
-    result += arguments[i];
+  for (let i = 0; i < arguments[0].length; i++) {
+    result += arguments[0][i];
   }
-	return +result;
+	return result;
 };
 
-const multiply = function() {
-
+const multiply = function() { 
+  let result = arguments[0][0];
+  for (let i = 1; i < arguments[0].length; i++) {
+    result *= arguments[0][i];
+  }
+	return result;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	let result = x ** y;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let result = 1;
+  for (let i = 2; i <= x; i++) {
+    result *= i; 
+  }
+  return result;
 };
 
 // Do not edit below this line
