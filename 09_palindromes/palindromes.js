837c1cc5b3a5489c395c697e2e7f06ecceb20964
ravenvst <142888381+ravenvst@users.noreply.github.com>
2023-12-21 11:21:40 +0100

    pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace("!", "");
+    str = str.replace(".", "");
+    str = str.replace(/,/g, "");
+    str = str.replace(/ /g, ""); 
+    str = str.toUpperCase();
+    console.log(str);
 
+    for (let i = 0; i < str.length/2; i++) {
+        if (str[i] !== str[str.length - i - 1]) {
+            console.log(str[i]);
+            console.log(str[str.length - i - 1]);
+            return false;
+            
+        } else if (str[i] === str[str.length - i - 1]) {
+            if (i >= str.length/2 -1) {
+                return true;
+            } else {
+                continue;
+            }
+        }
+        
+    }
 };
 
 // Do not edit below this line
