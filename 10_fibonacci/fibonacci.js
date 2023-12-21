d1c82665c775f1330c4dd6c38f5208af3df192a0
ravenvst <142888381+ravenvst@users.noreply.github.com>
2023-12-21 12:18:41 +0100

    pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(enteredChosenNumber) {
+   
+    let chosenNumber = Number(enteredChosenNumber);
+    
+    if (chosenNumber < 0) {
+        return "OOPS";
+    }
 
+    if (chosenNumber === 1) {
+        return 1;
+    }
+    
+    let x = 0;
+    let y = 1;
+
+    for (let i = 1; i < chosenNumber; i++) {
+        let temp = x + y;
+        if (i === chosenNumber - 1) {
+            return temp;
+        }
+        x = y;
+        y = temp;
+    }
 };
 
 // Do not edit below this line
