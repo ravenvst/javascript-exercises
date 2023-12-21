6e47b1f24727aa72e068b7118f06c97f9697d42f
ravenvst <142888381+ravenvst@users.noreply.github.com>
2023-12-21 14:00:26 +0100

    pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(ppl) {
+    let oldest = ppl.reduce(chooseOldest);
 
+    function chooseOldest (oldestSoFar, current) {
+        if (current.yearOfDeath == undefined) {
+            const d = new Date();
+            current.yearOfDeath = d.getFullYear(); 
+        }
+        if (oldestSoFar.yearOfDeath == undefined) {
+            const d = new Date();
+            oldestSoFar.yearOfDeath = d.getFullYear(); 
+        }
+        if ((oldestSoFar.yearOfDeath - oldestSoFar.yearOfBirth) > (current.yearOfDeath - current.yearOfBirth)) {
+            return oldestSoFar;
+        } else {
+            return current;
+        }
+    }
+        
+    return oldest;   
+    
+    
 };
 
 // Do not edit below this line
